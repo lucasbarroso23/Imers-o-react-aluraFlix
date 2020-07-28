2445cce9783862e6a5107bc9ca3d9fc47d22ebae
Lucas Barroso <lucasbarroso23@gmail.com>
2020-07-27 21:43:01 -0300

    Finalizando primeira aula, foram incluidos componentes mágicamente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import dadosIniciais from './data/dados_iniciais.json';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -7,7 +11,38 @@ function App() {
     <div style={{ background: "#414141" }}>
       <Menu />
 
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end"}
+      />
 
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />
+
+      <Footer />
 
     </div>
   );
