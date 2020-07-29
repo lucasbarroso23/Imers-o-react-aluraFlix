a838a9f3a03012364045355b6d59c701fd639a98
Lucas Barroso <lucasbarroso23@gmail.com>
2020-07-28 22:23:44 -0300

    adicionando rotas a aplicação

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function CadastroVideo() {
+    return (
+        <div>
+            pagina de cadastro de video
+        </div>
+    )
+}
 
 ReactDOM.render(
 
+    <BrowserRouter>
+        <Switch>
+            <Route path="/cadastro/video" exact component={CadastroVideo} />
+            <Route path="/" exact component={App} />
+            <Route component={() => (<div>pagina 404</div>)} />
 
-    <React.StrictMode >
-
-        <App />
-
-    </React.StrictMode>,
+        </Switch>
+    </BrowserRouter>,
 
     document.getElementById('root')
 
